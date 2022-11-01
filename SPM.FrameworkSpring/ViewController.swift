bbd7214a6475ba3463e58dd45288328c6e99648f
YuriyGubin <iosdevelop.2022@gmail.com>
2022-11-01 23:39:49 +0500

    End off lesson

--- SPM.FrameworkSpring/ViewController.swift
+++ SPM.FrameworkSpring/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
     
     @IBOutlet var coreAnimationView: UIView!
+    @IBOutlet var springAnimationView: SpringView!
     
     private var animationStarted = false
  
@@ -18,15 +20,23 @@ class ViewController: UIViewController {
         UIView.animate(
             withDuration: 0.5,
             delay: 0,
-            options: [.autoreverse, .repeat]
-        ) { [ unowned self ] in
-            if !animationStarted {
-                coreAnimationView.frame.origin.x -= 40
-                animationStarted.toggle()
+            options: [.autoreverse, .repeat]) { [ unowned self ] in
+                if !animationStarted {
+                    coreAnimationView.frame.origin.x -= 40
+                    animationStarted.toggle()
+                }
             }
-        }
         sender.pulsate()
     }
     
+    @IBAction func startSpringAnimation(_ sender: SpringButton) {
+        springAnimationView.animation = "shake"
+        springAnimationView.curve = "easeOut"
+        springAnimationView.force = 2
+        springAnimationView.animate()
+        
+        sender.animation = "squeeze"
+        sender.animate()
+    }
 }
 
