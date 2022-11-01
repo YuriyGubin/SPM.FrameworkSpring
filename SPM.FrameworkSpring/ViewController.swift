33e4d19af24ae3908291f5aa9a518bf35d4050d9
GubinYuriy <iosdevelop.2022@gmail.com>
2022-11-01 15:47:31 +0500

    Add animation for view

--- SPM.FrameworkSpring/ViewController.swift
+++ SPM.FrameworkSpring/ViewController.swift
@@ -11,8 +11,20 @@ class ViewController: UIViewController {
 
     
     @IBOutlet var coreAnimationView: UIView!
-
+    
+    private var animationStarted = false
+ 
     @IBAction func startCoreAnimation(_ sender: UIButton) {
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: [.autoreverse, .repeat]
+        ) { [ unowned self ] in
+            if !animationStarted {
+                coreAnimationView.frame.origin.x -= 40
+                animationStarted.toggle()
+            }
+        }
         sender.pulsate()
     }
     
